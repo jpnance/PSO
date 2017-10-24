e62e845611f7fcefee7d905e60339332e7f111e5
Patrick Nance <jpnance@gmail.com>
2017-10-24 14:42:42 -0700

    Adding the little script that generates everybody's results assuming they get a win this week.

---
+++ simulator/percentages.sh
@@ -0,0 +1,16 @@
+N=10000
+WEEK=8
+
+nodejs index.js n=$N
+nodejs index.js n=$N winners="$WEEK:Brett/Luke"
+nodejs index.js n=$N winners="$WEEK:Daniel"
+nodejs index.js n=$N winners="$WEEK:James/Charles"
+nodejs index.js n=$N winners="$WEEK:John/Zach"
+nodejs index.js n=$N winners="$WEEK:Keyon"
+nodejs index.js n=$N winners="$WEEK:Koci/Mueller"
+nodejs index.js n=$N winners="$WEEK:Mitch"
+nodejs index.js n=$N winners="$WEEK:Patrick"
+nodejs index.js n=$N winners="$WEEK:Quinn"
+nodejs index.js n=$N winners="$WEEK:Schex/Jeff"
+nodejs index.js n=$N winners="$WEEK:Syed/Terence"
+nodejs index.js n=$N winners="$WEEK:Trevor"
