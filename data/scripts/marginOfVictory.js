e86b93aa9b5579a4998ccd62eb2576944552786f
Patrick Nance <jpnance@gmail.com>
2018-11-12 07:38:06 -0800

    Adding a script for margin of victory.

---
+++ data/scripts/marginOfVictory.js
@@ -0,0 +1,17 @@
+var marginOfVictoryMap = function() {
+	var key = [this.season, this.week, this.home.name, this.away.name].join('-');
+
+	emit(key, Math.abs(this.home.score - this.away.score) * (this.season < 2012 ? 0.1 : 1));
+};
+
+var marginOfVictoryReduce = function(key, results) {
+	return results;
+};
+
+var marginOfVictoryQuery = {
+	type: 'regular',
+	'away.score': { '$exists': true },
+	'home.score': { '$exists': true }
+};
+
+db.games.mapReduce(marginOfVictoryMap, marginOfVictoryReduce, { out: 'marginOfVictory', query: marginOfVictoryQuery, sort: { season: 1, week: 1 } });
