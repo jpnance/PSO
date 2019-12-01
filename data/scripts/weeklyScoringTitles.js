d60b2283ed0c3b9ea81c0737d4c7ec8e5d9c1309
Patrick Nance <jpnance@gmail.com>
2019-12-01 14:51:32 -0800

    Adding a simple script for weekly scoring titles by regime.

---
+++ data/scripts/weeklyScoringTitles.js
@@ -0,0 +1,48 @@
+var weeklyScoringTitlesMap = function() {
+	var regimes = {
+		'Charles': 'James/Charles',
+		'Jake/Luke': 'Brett/Luke',
+		'John': 'John/Zach',
+		'Koci': 'Koci/Mueller',
+		'Pat/Quinn': 'Patrick',
+		'Schex/Jeff': 'Schex',
+		'Schexes': 'Schex',
+		'Syed': 'Syed/Kuan',
+		'Syed/Terence': 'Syed/Kuan'
+	};
+
+	var winner, loser;
+
+	if (this.away.score > this.home.score) {
+		winner = this.away;
+		loser = this.home;
+	}
+	else if (this.home.score > this.away.score) {
+		winner = this.home;
+		loser = this.away;
+	}
+
+	if (winner.record.allPlay.week.losses == 0) {
+		var key = regimes[winner.name] || winner.name;
+
+		emit(key, 1);
+	}
+};
+
+var weeklyScoringTitlesReduce = function(key, results) {
+	var scoringTitles = 0;
+
+	results.forEach(result => {
+		scoringTitles += result;
+	});
+
+	return scoringTitles;
+};
+
+var weeklyScoringTitlesQuery = {
+	type: 'regular',
+	'away.score': { '$exists': true },
+	'home.score': { '$exists': true }
+};
+
+db.games.mapReduce(weeklyScoringTitlesMap, weeklyScoringTitlesReduce, { out: 'weeklyScoringTitles', query: weeklyScoringTitlesQuery, sort: { season: 1, week: 1 } });
