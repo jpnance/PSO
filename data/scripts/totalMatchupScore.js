4b19ffcac7868f252d5a77592ebbcbe24f3a85aa
Patrick Nance <jpnance@gmail.com>
2019-12-13 10:11:08 -0800

    A script that calculates total matchup scores.

---
+++ data/scripts/totalMatchupScore.js
@@ -0,0 +1,20 @@
+var totalMatchupScoreMap = function() {
+	var totalMatchupScore = this.away.score + this.home.score;
+
+	if (this.season < 2012) {
+		totalMatchupScore /= 10;
+	}
+
+	emit(this.season + '-' + this.week + '-' + this.away.name + '-' + this.home.name, totalMatchupScore);
+};
+
+var totalMatchupScoreReduce = function(key, results) {
+};
+
+var totalMatchupScoreQuery = {
+	type: 'regular',
+	'away.score': { '$exists': true },
+	'home.score': { '$exists': true }
+};
+
+db.games.mapReduce(totalMatchupScoreMap, totalMatchupScoreReduce, { out: 'totalMatchupScore', query: totalMatchupScoreQuery, sort: { season: 1, week: 1 } });
