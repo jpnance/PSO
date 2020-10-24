d882456485fadf63fb12c66421a1bbb86f29e625
Patrick Nance <jpnance@gmail.com>
2020-10-23 18:00:25 -0700

    Okay, this definitely works. I think it's pretty ugly but, hey, what else is new with this stuff?

--- public/simulator.js
+++ public/simulator.js
@@ -1,6 +1,32 @@
-$(document).ready(function() {
-	$.post('/simulator', function(data) {
+var gatherConditions = function() {
+	var conditions = [];
+
+	$('input:checked').each(function() {
+		var $this = $(this);
+		var condition = $this.val();
+
+		if (condition != 'vs') {
+			conditions.push(condition);
+		}
+	});
+
+	return conditions;
+};
+
+var replaceTable = function() {
+	var conditions = gatherConditions();
+	console.log(conditions);
+
+	$.post('/simulator/' + conditions.join(','), function(data) {
 		$('table').replaceWith(data);
 		$('[data-toggle="tooltip"]').tooltip();
+	});
+};
+
+$(document).ready(function() {
+	replaceTable();
+
+	$('div.btn-group').on('click', 'input', function(e) {
+		$('table').animate({ opacity: 0.3 }, 100, 'linear', replaceTable);
 	});
 });
