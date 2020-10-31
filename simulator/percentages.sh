c0eef8dc3e92ba3a5ec7b8d2eac2a99ed72ff5e6
Patrick Nance <jpnance@gmail.com>
2020-10-30 17:33:17 -0700

    More tidiness.

--- simulator/percentages.sh
+++ simulator/percentages.sh
@@ -2,15 +2,15 @@ N=10000
 WEEK=8
 
 nodejs index.js n=$N
-nodejs index.js n=$N winners="$WEEK:Brett/Luke"
+nodejs index.js n=$N winners="$WEEK:Brett"
 nodejs index.js n=$N winners="$WEEK:James/Charles"
 nodejs index.js n=$N winners="$WEEK:John/Zach"
 nodejs index.js n=$N winners="$WEEK:Keyon"
 nodejs index.js n=$N winners="$WEEK:Koci/Mueller"
+nodejs index.js n=$N winners="$WEEK:Luke"
 nodejs index.js n=$N winners="$WEEK:Mitch"
 nodejs index.js n=$N winners="$WEEK:Patrick"
 nodejs index.js n=$N winners="$WEEK:Quinn"
 nodejs index.js n=$N winners="$WEEK:Schex"
-nodejs index.js n=$N winners="$WEEK:Syed/Kuan"
 nodejs index.js n=$N winners="$WEEK:Terence"
 nodejs index.js n=$N winners="$WEEK:Trevor"
