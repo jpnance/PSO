8eecaa22b8b2a30d9121df78ba0fcbe9eeea2432
Patrick Nance <jpnance@gmail.com>
2021-01-01 15:18:10 -0800

    Adding a regimes file that we can include in some of these other scripts.

---
+++ data/scripts/regimes.js
@@ -0,0 +1,12 @@
+var regimes = {
+	'Charles': 'James/Charles',
+	'Brett/Luke': 'Luke',
+	'Jake/Luke': 'Luke',
+	'John': 'John/Zach',
+	'Koci': 'Koci/Mueller',
+	'Pat/Quinn': 'Patrick',
+	'Schex/Jeff': 'Schex',
+	'Schexes': 'Schex',
+	'Syed': 'Syed/Kuan',
+	'Syed/Terence': 'Syed/Kuan'
+};
