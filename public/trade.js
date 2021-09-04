b5647b05316b1f629ee47df86a6eb9df8a36c7fa
Patrick Nance <jpnance@gmail.com>
2021-09-04 08:28:46 -0700

    Some basic interaction.

--- public/trade.js
+++ public/trade.js
@@ -1,2 +1,11 @@
 $(document).ready(function() {
+	$('.form-check-input:checked').each((i, input) => {
+		var franchiseId = input.id;
+		$('div[id=gets-' + franchiseId).removeClass('d-none');
+	});
+
+	$('.form-check').on('click', '.form-check-input', (e) => {
+		var franchiseId = e.currentTarget.id;
+		$('div[id=gets-' + franchiseId).toggleClass('d-none');
+	});
 });
