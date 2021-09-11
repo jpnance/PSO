ae816f9d712dbb7db8faffdd1012fe020a0b5cbd
Patrick Nance <jpnance@gmail.com>
2021-09-11 10:53:36 -0700

    Making everything.sh a little more self-sufficient.

--- everything.sh
+++ everything.sh
@@ -20,13 +20,16 @@ cd ..
 cd ..
 
 cd h2h
+mkdir -p ../public/h2h
 node index.js render
 cd ..
 
 cd history
+mkdir -p ../public/history
 node index.js render
 cd ..
 
 cd jaguar
+mkdir -p ../public/jaguar
 node index.js render
 cd ..
