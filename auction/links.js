a2dbd1c4c41db117ba26ad3909bf39b994cb426e
Patrick Nance <jpnance@gmail.com>
2021-10-13 14:49:27 -0700

    Better.

--- auction/links.js
+++ auction/links.js
@@ -1,7 +1,13 @@
 const dotenv = require('dotenv').config({ path: __dirname + '/../.env' });
 
-const auctionKeys = JSON.parse(process.env.AUCTION_USERS);
+const auctionKeys = {};
+const auctionUsers = JSON.parse(process.env.AUCTION_USERS);
+const owners = JSON.parse(process.env.NOMINATION_ORDER);
 
-Object.keys(auctionKeys).forEach((key) => {
-	console.log(auctionKeys[key] + ': https://thedynastyleague.com/auction/login/' + key);
+Object.keys(auctionUsers).forEach((key) => {
+	auctionKeys[auctionUsers[key]] = key;
+});
+
+owners.sort().forEach((owner) => {
+	console.log(owner + ': https://thedynastyleague.com/auction/login/' + auctionKeys[owner]);
 });
