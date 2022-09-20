747876470b49d8efdf3561e354a44f074102333f
Patrick Nance <jpnance@gmail.com>
2022-09-20 08:40:13 -0700

    Migrating seasonSummaries.

--- data/scripts/seasonSummaries.js
+++ data/scripts/seasonSummaries.js
@@ -1,60 +1,77 @@
-var seasonSummariesMap = function() {
-	['away', 'home'].forEach((ownerType) => {
-		//var seasonOwner = { season: this.season, this[ownerType].name };
-		var seasonOwner = this.season + ' ' + this[ownerType].name;
-		var data = {
-			scores: [ this[ownerType].score ],
-			records: [ this[ownerType].record.straight.cumulative.wins + '-' + this[ownerType].record.straight.cumulative.losses + '-' + this[ownerType].record.straight.cumulative.ties ],
-			playoffs: this.type == 'semifinal',
-			titleGame: this.type == 'championship',
-			champion: this.type == 'championship' && this.winner.name == this[ownerType].name
-		};
+const dotenv = require('dotenv').config({ path: __dirname + '/../../.env' });
 
-		emit(seasonOwner, data);
-	});
-};
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-var seasonSummariesReduce = function(key, results) {
-	var sumReducer = (a, b) => parseInt(a) + parseInt(b);
+const Game = require('../models/Game');
 
-	var seasonSummary = { scores: [], records: [], playoffs: false, titleGame: false, champion: false };
+Game.mapReduce({
+	map: function() {
+		['away', 'home'].forEach((ownerType) => {
+			const seasonOwner = this.season + ' ' + this[ownerType].name;
+			const data = {
+				scores: [ this[ownerType].score ],
+				records: [ this[ownerType].record.straight.cumulative.wins + '-' + this[ownerType].record.straight.cumulative.losses + '-' + this[ownerType].record.straight.cumulative.ties ],
+				playoffs: this.type == 'semifinal',
+				titleGame: this.type == 'championship',
+				champion: this.type == 'championship' && this.winner.name == this[ownerType].name
+			};
 
-	results.forEach((result) => {
-		result.scores.forEach((score) => {
-			seasonSummary.scores.push(score);
+			emit(seasonOwner, data);
+		});
+	},
+
+	reduce: function(key, results) {
+		const sumReducer = (a, b) => parseInt(a) + parseInt(b);
+
+		let seasonSummary = { scores: [], records: [], playoffs: false, titleGame: false, champion: false };
+
+		results.forEach((result) => {
+			result.scores.forEach((score) => {
+				seasonSummary.scores.push(score);
+			});
+
+			result.records.forEach((record) => {
+				seasonSummary.records.push(record);
+			});
+
+			if (result.playoffs) {
+				seasonSummary.playoffs = true;
+			}
+
+			if (result.titleGame) {
+				seasonSummary.titleGame = true;
+			}
+
+			if (result.champion) {
+				seasonSummary.champion = true;
+			}
 		});
 
-		result.records.forEach((record) => {
-			seasonSummary.records.push(record);
+		seasonSummary.records.sort((a, b) => {
+			const aGames = a.split('-').reduce(sumReducer);
+			const bGames = b.split('-').reduce(sumReducer);
+
+			return a - b;
 		});
 
-		if (result.playoffs) {
-			seasonSummary.playoffs = true;
-		}
+		return seasonSummary;
+	},
 
-		if (result.titleGame) {
-			seasonSummary.titleGame = true;
-		}
+	out: 'seasonSummaries',
 
-		if (result.champion) {
-			seasonSummary.champion = true;
-		}
-	});
+	query: {
+		'away.score': { '$exists': true },
+		'home.score': { '$exists': true },
+		'type': { '$ne': 'consolation' }
+	},
 
-	seasonSummary.records.sort((a, b) => {
-		var aGames = a.split('-').reduce(sumReducer);
-		var bGames = b.split('-').reduce(sumReducer);
-
-		return a - b;
-	});
-
-	return seasonSummary;
-};
-
-var seasonSummariesQuery = {
-	'away.score': { '$exists': true },
-	'home.score': { '$exists': true },
-	'type': { '$ne': 'consolation' }
-};
-
-db.games.mapReduce(seasonSummariesMap, seasonSummariesReduce, { out: 'seasonSummaries', query: seasonSummariesQuery, sort: { season: 1, week: 1 } });
+	sort: {
+		season: 1,
+		week: 1
+	}
+}).then((data) => {
+	mongoose.disconnect();
+	process.exit();
+});
