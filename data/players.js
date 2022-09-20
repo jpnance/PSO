6fde85d990c0b5060b605ab016a367a68473b7d4
Patrick Nance <jpnance@gmail.com>
2022-09-20 14:04:22 -0700

    Stray line break.

--- data/players.js
+++ data/players.js
@@ -9,7 +9,6 @@ const siteData = {
 	sheetLink: 'https://sheets.googleapis.com/v4/spreadsheets/1nas3AqWZtCu_UZIV77Jgxd9oriF1NQjzSjFWW86eong/values/Rostered',
 };
 
-
 var newSheetsPromise = function() {
 	return new Promise(function(resolve, reject) {
 		request
