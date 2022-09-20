3f1dae8a544eebb9100d1d0f9ea762d61136dca4
Patrick Nance <jpnance@gmail.com>
2022-09-20 08:34:41 -0700

    Migrating regularSeasonWins.

--- data/scripts/regularSeasonWins.js
+++ data/scripts/regularSeasonWins.js
@@ -1,50 +1,57 @@
-load('./regimes.js');
+const dotenv = require('dotenv').config({ path: __dirname + '/../../.env' });
 
-var regularSeasonWinsMap = function() {
-	var winner, loser;
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-	if (this.away.score > this.home.score) {
-		winner = this.away;
-		loser = this.home;
-	}
-	else if (this.home.score > this.away.score) {
-		winner = this.home;
-		loser = this.away;
-	}
+const Game = require('../models/Game');
+const regimes = require('./regimes');
 
-	var key = regimes[this.winner.name] || this.winner.name;
+Game.mapReduce({
+	map: function() {
+		let winner, loser;
 
-	emit(key, 1);
-};
-
-var regularSeasonWinsReduce = function(key, results) {
-	var wins = 0;
-
-	results.forEach(result => {
-		wins += result;
-	});
-
-	return wins;
-};
-
-var regularSeasonWinsQuery = {
-	type: 'regular',
-	'away.score': { '$exists': true },
-	'home.score': { '$exists': true }
-};
-
-db.games.mapReduce(
-	regularSeasonWinsMap,
-	regularSeasonWinsReduce,
-	{
-		out: 'regularSeasonWins',
-		query: regularSeasonWinsQuery,
-		sort: {
-			season: 1,
-			week: 1
-		},
-		scope: {
-			regimes: regimes
+		if (this.away.score > this.home.score) {
+			winner = this.away;
+			loser = this.home;
 		}
+		else if (this.home.score > this.away.score) {
+			winner = this.home;
+			loser = this.away;
+		}
+
+		const key = regimes[this.winner.name] || this.winner.name;
+
+		emit(key, 1);
+	},
+
+	reduce: function(key, results) {
+		var wins = 0;
+
+		results.forEach(result => {
+			wins += result;
+		});
+
+		return wins;
+	},
+
+	out: 'regularSeasonWins',
+
+	query: {
+		type: 'regular',
+		'away.score': { '$exists': true },
+		'home.score': { '$exists': true }
+	},
+
+	sort: {
+		season: 1,
+		week: 1
+	},
+
+	scope: {
+		regimes: regimes
 	}
-);
+}).then((data) => {
+	mongoose.disconnect();
+	process.exit();
+});
