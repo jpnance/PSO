5f0e0ac89ab32103d571bd7af1d7fae2a91fb278
Patrick Nance <jpnance@gmail.com>
2022-09-20 08:18:38 -0700

    Migrating marginOfVictory.

--- data/scripts/marginOfVictory.js
+++ data/scripts/marginOfVictory.js
@@ -1,17 +1,40 @@
-var marginOfVictoryMap = function() {
-	var key = [this.season, this.week, this.winner.name, this.loser.name].join('-');
+const dotenv = require('dotenv').config({ path: __dirname + '/../../.env' });
 
-	emit(key, Math.abs(this.home.score - this.away.score) * (this.season < 2012 ? 0.1 : 1));
-};
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-var marginOfVictoryReduce = function(key, results) {
-	return results;
-};
+const Game = require('../models/Game');
+const regimes = require('./regimes');
 
-var marginOfVictoryQuery = {
-	type: 'regular',
-	'away.score': { '$exists': true },
-	'home.score': { '$exists': true }
-};
+Game.mapReduce({
+	map: function() {
+		const key = [this.season, this.week, this.winner.name, this.loser.name].join('-');
 
-db.games.mapReduce(marginOfVictoryMap, marginOfVictoryReduce, { out: 'marginOfVictory', query: marginOfVictoryQuery, sort: { season: 1, week: 1 } });
+		emit(key, Math.abs(this.home.score - this.away.score) * (this.season < 2012 ? 0.1 : 1));
+	},
+
+	reduce: function(key, results) {
+		return results;
+	},
+
+	out: 'marginOfVictory',
+
+	query: {
+		type: 'regular',
+		'away.score': { '$exists': true },
+		'home.score': { '$exists': true }
+	},
+
+	sort: {
+		season: 1,
+		week: 1
+	},
+
+	scope: {
+		regimes: regimes
+	}
+}).then((data) => {
+	mongoose.disconnect();
+	process.exit();
+});
