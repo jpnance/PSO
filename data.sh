a705247ff94efd67c8df37052a888ada7103d8c7
Patrick Nance <jpnance@gmail.com>
2022-09-21 11:30:34 -0700

    Create the trade directory if it doesn't exist.

--- data.sh
+++ data.sh
@@ -8,5 +8,7 @@ node cash.js > ../public/data/cash.json
 node players.js > ../public/data/players.json
 node picks.js > ../public/data/picks.json
 cd ..
+
 cd trade
+mkdir -p ../public/trade
 node index.js render
