8bcd4d0b73c1552adab00b2b4d761d9809b34b17
Patrick Nance <jpnance@gmail.com>
2022-12-20 09:39:22 -0800

    Might as well do 100,000 trials, right? Also updating the numbers for 2022.

--- simulator/matchup.sh
+++ simulator/matchup.sh
@@ -1,9 +1,9 @@
-owners=("Keyon:162.60,26.53" "Schex:160.29,25.66" "Jason:154.95,28.83" "James/Charles:161.18,21.56")
+owners=("Luke:153.03,16.60" "Keyon:149.23,24.04" "Jason:155.88,17.72" "Mitch/Mike:160.49,23.28")
 
 for i in $(seq 0 $((${#owners[@]} - 1))); do
 	for j in $(seq $i $((${#owners[@]} - 1))); do
 		if (( $i != $j )); then
-			node matchup.js n=10000 owners="${owners[$i]};${owners[$j]}"
+			node matchup.js n=100000 owners="${owners[$i]};${owners[$j]}"
 			echo
 		fi
 	done
