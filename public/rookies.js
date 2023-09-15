4fb574c8c77f2a47775e39eb26e9c715ef5801f8
Patrick Nance <jpnance@gmail.com>
2023-09-14 21:13:02 -0700

    This is very silly functionality but, hey, it's fixed now.

--- public/rookies.js
+++ public/rookies.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
+	var query = location.search.substring(1).split(/=/);
+
+	if (query.length > 1 && query[1].match(/^20\d\d/)) {
+		window.history.replaceState({ season: query[1] }, 'Rookie Salaries for ' + query[1] + '?season=' + query[1]);
+
+		displaySeason(query[1]);
+	}
+
 	$('select').on('change', function(e) {
 		var $this = $(this);
 
-		window.history.replaceState({ season: $this.val() }, 'Rookie Salaries for ' + $this.val(), '?season=' + $this.val());
+		window.history.pushState({ season: $this.val() }, 'Rookie Salaries for ' + $this.val(), '?season=' + $this.val());
 
 		displaySeason($this.val());
 	});
