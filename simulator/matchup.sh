35e06a041ec93a3ebefd48240ccf9f9336d00536
Patrick Nance <jpnance@gmail.com>
2024-12-18 08:09:19 -0800

    Stats for 2024.

--- simulator/matchup.sh
+++ simulator/matchup.sh
@@ -1,4 +1,6 @@
-owners=("Koci/Mueller:152.12,25.72" "Justin:142.62,16.15" "Mike:143.73,21.16" "Mitch:144.03,22.76")
+#!/bin/bash
+
+owners=("Keyon:163.55,21.74" "Schexes:156.24,17.75" "Luke:146.92,23.87" "Koci/Mueller:157.92,40.34")
 
 for i in $(seq 0 $((${#owners[@]} - 1))); do
 	for j in $(seq $i $((${#owners[@]} - 1))); do
