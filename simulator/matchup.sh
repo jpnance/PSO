0c56e354feeca3047c346fb558caad9937de1b09
Patrick Nance <jpnance@gmail.com>
2025-12-23 12:22:36 -0800

    Numbers for the championship game.

--- simulator/matchup.sh
+++ simulator/matchup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-owners=("Quinn:160.77,25.84" "Schexes:155.61,21.07" "Koci/Mueller:156.05,22.33" "Jason:144.13,28.22")
+owners=("Quinn:160.00,25.15" "Schexes:154.90,20.55" "Koci/Mueller:159.30,25.19" "Jason:145.09,27.53")
 
 for i in $(seq 0 $((${#owners[@]} - 1))); do
 	for j in $(seq $i $((${#owners[@]} - 1))); do
