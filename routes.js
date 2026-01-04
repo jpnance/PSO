f420aa7ac221c9de6863b8791ca43181cacfec33
Patrick Nance <jpnance@gmail.com>
2026-01-04 00:24:06 -0800

    Hooking up admin routes (whoops) but also being smarter about which dead period we're conditioning display on.

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ var auction = require('./auction/service.js');
 var simulator = require('./simulator/service.js');
 var scuttlebot = require('./scuttlebot/service.js');
 var league = require('./league/service.js');
+var admin = require('./admin/service.js');
 
 module.exports = function(app) {
 	app.get('/', function(request, response) {
@@ -23,4 +24,9 @@ module.exports = function(app) {
 	});
 	app.get('/scuttlebot', scuttlebot.prompt);
 	app.post('/scuttlebot/message', scuttlebot.postMessage);
+	
+	// Admin routes
+	app.get('/admin', admin.configPage);
+	app.post('/admin/config', admin.updateConfig);
+	app.post('/admin/config/advance-season', admin.advanceSeason);
 };
