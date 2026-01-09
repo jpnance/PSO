a348f3dfce9a430921c4fddfaf97ab73eaa3c6ab
Patrick Nance <jpnance@gmail.com>
2026-01-08 16:23:10 -0800

    This is no longer the way.

--- data.sh
+++ data.sh
@@ -8,8 +8,3 @@ node schedule.js > ../public/data/schedule.json
 node cash.js > ../public/data/cash.json
 node players.js > ../public/data/players.json
 node picks.js > ../public/data/picks.json
-cd ..
-
-cd trade
-mkdir -p ../public/trade
-node index.js render
