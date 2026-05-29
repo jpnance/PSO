f8efaedee95835c3b5d3cd0ceca6c7b6ec528262
Patrick Nance <jpnance@gmail.com>
2026-05-29 13:22:28 -0700

    Brevity.

--- helpers/nav.js
+++ helpers/nav.js
@@ -130,8 +130,8 @@ function buildNav(options) {
 		items: [
 			{ label: 'Rookie Draft', icon: 'fa-list-ol', href: '/draft', active: isActive('draft') },
 			{ label: 'Rookie Salaries', icon: 'fa-graduation-cap', href: '/rookies', active: isActive('rookies') },
-			{ label: 'Restricted Free Agents', icon: 'fa-user-plus', href: '/rfa', active: isActive('rfa') },
-			{ label: 'Unrestricted Free Agents', icon: 'fa-user-o', href: '/ufa', active: isActive('ufa') },
+			{ label: 'RFAs', icon: 'fa-user-plus', href: '/rfa', active: isActive('rfa') },
+			{ label: 'UFAs', icon: 'fa-user-o', href: '/ufa', active: isActive('ufa') },
 			{ label: 'Free Agent Auction', icon: 'fa-gavel', href: '#', soon: true }
 		]
 	});
